efe62814dc32d50e069a0402f598e2ac837c4c0c
Bujin Wang <bujinwang@gmail.com>
2025-03-19 18:24:15 -0600

    fly.io 127.0.0.1:8080 error

---
+++ clinicwebapp/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Enable compression
+app.use(compression());
+
+// Enable CORS
+app.use(cors());
+
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// For any request that doesn't match one above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Listen on all network interfaces
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on 0.0.0.0:${PORT}`);
+}); 
